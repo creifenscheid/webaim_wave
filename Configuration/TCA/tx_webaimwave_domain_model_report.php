e099a44f4e05229742fee4216f6027efe0ea9dfa
Christian Reifenscheid <dev@christian-reifenscheid.de>
2022-07-29 08:30:36 +0200

    [TASK] Model
    WIP - Implements TCA

---
+++ Configuration/TCA/tx_webaimwave_domain_model_report.php
@@ -0,0 +1,82 @@
+<?php
+
+return [
+    'ctrl' => [
+        #'hideTable' => true,
+        'adminOnly' => true,
+        'tstamp' => 'tstamp',
+        'crdate' => 'crdate',
+        'cruser_id' => 'cruser_id',
+        'origUid' => 't3_origuid',
+        'default_sortby' => 'crdate DESC',
+        
+        'transOrigPointerField' => 'l18n_parent',
+        'transOrigDiffSourceField' => 'l18n_diffsource',
+        'languageField' => 'sys_language_uid',
+        'translationSource' => 'l10n_source',
+        
+        'delete' => 'delete',
+        'enablecolumns' => [
+            'disabled' => 'hidden',
+        ],
+    ],
+    'types' => [
+        [
+            'showitem' => 'xxx',
+        ]
+    ],
+    'columns' => [
+        'hidden' => [
+            'exclude' => true,
+            'label' => 'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.enabled',
+            'config' => [
+                'type' => 'check',
+                'renderType' => 'checkboxToggle',
+                'items' => [
+                    [
+                        0 => '',
+                        1 => '',
+                        'invertStateDisplay' => true
+                    ]
+                ]
+            ]
+        ],
+        sys_language_uid' => [
+            'exclude' => true,
+            'label' => 'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.language',
+            'config' => [
+                'type' => 'language',
+            ],
+        ],
+        'l18n_parent' => [
+            'displayCond' => 'FIELD:sys_language_uid:>:0',
+            'label' => 'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.l18n_parent',
+            'config' => [
+                'type' => 'select',
+                'renderType' => 'selectSingle',
+                'items' => [
+                    [
+                        '',
+                        0,
+                    ],
+                ],
+                'foreign_table' => 'tx_webaimwave_domain_model_report',
+                'foreign_table_where' =>
+                    'AND {#tx_webaimwave_domain_model_report}.{#pid}=###CURRENT_PID###'
+                    . ' AND {#tx_webaimwave_domain_model_report}.{#sys_language_uid} IN (-1,0)',
+                'default' => 0,
+            ],
+        ],
+        'l10n_source' => [
+            'config' => [
+                'type' => 'passthrough',
+            ],
+        ],
+        'l18n_diffsource' => [
+            'config' => [
+                'type' => 'passthrough',
+                'default' => '',
+            ],
+        ],
+    ]
+];
